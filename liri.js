7a457e9384c7a2aad2072862e7ee3ed545fc4f8d
Ashley Waldner <ashleywaldner22@gmail.com>
2020-04-26 09:08:27 -0500

    switch statement

--- liri.js
+++ liri.js
@@ -1,7 +1,37 @@
+//required installations
 require("dotenv").config();
-
 var keys = require("./keys.js");
-var axios = require('axios');
+var fs = require("fs");
 var Spotify = require('node-spotify-api');
+var axios = require("axios");
+// var inquirer = require("inquirer");
+var bandsintown = require("bandsintown");
+var omdb = require("omdb");
+var moment = require('moment');
+moment().format();
 
-var spotify = new Spotify(keys.spotify);
+//vars 
+var action = process.argv[2]
+
+switch(action){
+    case "movie-this":
+        movie()
+        break;
+
+    case "concert-this":
+        band()
+        break;
+
+    case "spotify-this-song":
+        song()
+        break;
+
+    case "do-what-it-says":
+        says()
+        break;
+
+    default:
+        break;
+    
+}
+
