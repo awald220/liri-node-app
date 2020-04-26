12589f11cc1c530017475268f4847511c27932b2
Ashley Waldner <ashleywaldner22@gmail.com>
2020-04-26 12:31:47 -0500

    get data back from the spotify API

--- liri.js
+++ liri.js
@@ -12,38 +12,44 @@ var axios = require("axios");
 
 //vars 
 var action = process.argv[2]
+var functionData = process.argv[3]
+console.log(action, functionData)
 
 switch(action){
-    case "movie-this":
-        movie()
-        break;
+    // case "movie-this":
+    //     movie()
+    //     break;
 
-    case "concert-this":
-        band()
-        break;
+    // case "concert-this":
+    //     band()
+    //     break;
 
     case "spotify-this-song":
         song()
         break;
 
-    case "do-what-it-says":
-        says()
-        break;
+    // case "do-what-it-says":
+    //     says()
+    //     break;
 
     default:
         break;
     
 }
 
+//function that runs the spotify api
 function song(){
-    var spotify = new Spotify(keys.spotify);
+    var spotify = new Spotify({
+        id: "32f65d124f9246eb953de653e60f3c6b",
+        secret: "83bd22ae940340d7a78adde1a238deec"
+    });
 
     spotify.search({ type: 'track', query: functionData, limit: 1}, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
         // console.log(name)
-        // console.log(data.tracks)
+        console.log(data.tracks)
         for (var key in data.tracks.items){
             console.log(data.tracks.items[key].artists[0].name);
             console.log(data.tracks.items[key].preview_url);
@@ -54,3 +60,4 @@ function song(){
         
       });
 }
+
